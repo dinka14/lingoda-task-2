d0d3c9dab936eb07c1a7f4bb44ac3637ffda5303
denis <dinka14@yandex.ru>
2018-03-18 09:58:27 +0300

    move locators separately

--- search.py
+++ search.py
@@ -14,41 +14,46 @@ class SearchItem(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub',
                                        desired_capabilities=DesiredCapabilities.CHROME)
+        self.find_search_field = "twotabsearchtextbox"
+        self.search_string = "Batman comics"
+        self.find_first_item = "//ul[@id='s-results-list-atf']//li[@id='result_0']"
+        self.find_title = "//ul[@id='s-results-list-atf']//li[@id='result_0']//a[@class='a-link-normal " \
+                          "s-access-detail-page  s-color-twister-title-link a-text-normal']"
+        self.find_price = "//ul[@id='s-results-list-atf']//li[@id='result_0']//div[@class='a-fixed-left-grid-col " \
+                          "a-col-right']//div[@class='a-row']//a[@class='a-link-normal a-text-normal']//span[" \
+                          "@class='a-size-base a-color-price s-price a-text-bold']"
+        self.find_rating = "//ul[@id='s-results-list-atf']//li[@id='result_0']//span[@class='a-declarative']//span[" \
+                           "@class='a-icon-alt']"
+        self.find_title_second_page = "//div[@id='centerCol']//div[@id='booksTitle']//span[@id='productTitle']"
+        self.find_price_second_page = "//div[@id='tmmSwatches']//span[@class='a-size-base a-color-price a-color-price']"
 
     def test_search_in_main_page(self):
         stream_handler.stream = sys.stdout
         driver = self.driver
+
         logging.getLogger().info("\nGoing to https://www.amazon.de")
         driver.get("https://www.amazon.de")
         self.assertIn("Amazon.de", driver.title)
 
-        elem = driver.find_element_by_id("twotabsearchtextbox")
+        elem = driver.find_element_by_id(self.find_search_field)
 
         # Search for Batman comics
-        search_string = "Batman comics"
-        logging.getLogger().info('Search for "{0}"'.format(search_string))
-        elem.send_keys(search_string)
+        logging.getLogger().info('Search for "{0}"'.format(self.search_string))
+        elem.send_keys(self.search_string)
         elem.send_keys(Keys.RETURN)
 
         # Check that results number above 0
         logging.getLogger().info("Check that results number above 0")
-        self.assertTrue(driver.find_element_by_xpath("//ul[@id='s-results-list-atf']//li[@id='result_0']"))
+        self.assertTrue(driver.find_element_by_xpath(self.find_first_item))
 
-        title = driver.find_element_by_xpath("//ul[@id='s-results-list-atf']//li[@id='result_0']//"
-                                                     "a[@class='a-link-normal s-access-detail-page  "
-                                                     "s-color-twister-title-link a-text-normal']")
-
+        title = driver.find_element_by_xpath(self.find_title)
         title_text = title.text
 
         # Check that title contains "Batman" word
         logging.getLogger().info('Check that title contains "Batman" word')
         self.assertIn("Batman", str(title_text))
 
-        price_text = driver.find_element_by_xpath("//ul[@id='s-results-list-atf']//li[@id='result_0']//div["
-                                             "@class='a-fixed-left-grid-col a-col-right']//div[@class='a-row']//a["
-                                             "@class='a-link-normal a-text-normal']//span[@class='a-size-base "
-                                             "a-color-price s-price a-text-bold']").text
-
+        price_text = driver.find_element_by_xpath(self.find_price).text
         str_price = str(price_text)
         float_price = float(str_price[str_price.index("EUR") + len("EUR"):].replace(",", "."))
 
@@ -62,19 +67,14 @@ class SearchItem(unittest.TestCase):
 
         # Check that item has rating
         logging.getLogger().info('Check that item has rating')
-        self.assertTrue(driver.find_element_by_xpath("//ul[@id='s-results-list-atf']//li[@id='result_0']//span["
-                                              "@class='a-declarative']//span[@class='a-icon-alt']"))
-
-
+        self.assertTrue(driver.find_element_by_xpath(self.find_rating))
 
         # Open 1st item page
         logging.getLogger().info('Open 1st item page')
         title.click()
 
-        title_second_page_text = driver.find_element_by_xpath("//div[@id='centerCol']//div[@id='booksTitle']//span["
-                                                         "@id='productTitle']").text
-        price_second_page_text = driver.find_element_by_xpath("//div[@id='tmmSwatches']//span[@class='a-size-base "
-                                                         "a-color-price a-color-price']").text
+        title_second_page_text = driver.find_element_by_xpath(self.find_title_second_page).text
+        price_second_page_text = driver.find_element_by_xpath(self.find_price_second_page).text
 
         # Check that title and price are equal to the ones in result search
         logging.getLogger().info('Check that title and price are equal to the ones in result search')
